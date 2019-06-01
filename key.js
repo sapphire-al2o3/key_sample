e052e9e88c456484ef5b243d3e7d6c2163c7dca4
tel <t_telkun@hotmail.com>
2019-06-02 02:42:46 +0900

    add js file

---
+++ key.js
@@ -0,0 +1,75 @@
+let canvas,
+    ctx,
+    time = 0.0,
+    keyState = [],
+    ripple = [];
+
+function rgba(r, g, b, a) {
+    return `rgba(${r},${g},${b},${a})`;
+}
+
+function circlePos(k) {
+    k -= 'A'.charCodeAt(0);
+    const r = k * Math.PI * 2.0 / 26.0;
+    const x = 200 + 160 * Math.cos(r);
+    const  y = 200 + 160 * Math.sin(r);
+    return {x: x, y: y};
+}
+
+function interval(callback) {
+    const proc = t => {
+        callback(t);
+        window.requestAnimationFrame(proc);
+    };
+    proc(0);
+}
+
+function ready() {
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    ctx.font = '16pt Consolas';
+    document.onkeypress = e => {
+        return false;
+    };
+    document.onkeydown = e => {
+        if(!keyState[e.keyCode]) {
+            const p = circlePos(e.keyCode);
+            ripple.push({x: p.x, y: p.y, z: 0});
+        }
+        keyState[e.keyCode] = true;
+        return false;
+    };
+    document.onkeyup = e => {
+        keyState[e.keyCode] = false;
+        return false;
+    };
+
+    interval(t => {
+        ctx.clearRect(0, 0, 400, 400);
+        
+        ripple = ripple.filter(function(e) {
+            ctx.strokeStyle = rgba(0x44, 0xDD, 0xFF, 1.0 - e.z / 200.0);
+            ctx.beginPath();
+            ctx.arc(e.x, e.y, e.z, 0, Math.PI * 2, false);
+            ctx.stroke();
+            e.z += 2.5;
+            return e.z < 200.0;
+        });
+        
+        for(var i = 0; i < 26; i++) {
+            var r = i * Math.PI * 2.0 / 26.0;
+            var x = 200.0 + 160 * Math.cos(r);
+            var y = 200.0 + 160 * Math.sin(r);
+            var key = 'A'.charCodeAt(0) + i;
+            if(keyState[key]) {
+                ctx.font = '24pt Consolas';
+                ctx.fillStyle = '#EE4455';
+            } else {
+                ctx.font = '16pt Consolas';
+                ctx.fillStyle = '#444444';
+            }
+            ctx.fillText(String.fromCharCode('A'.charCodeAt(0) + i), x, y);
+        }
+        time += 1.0;
+    });
+}
